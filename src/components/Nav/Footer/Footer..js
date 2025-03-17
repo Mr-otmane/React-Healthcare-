77ba97ede4255cf0a3e23e2693509564a0fe04e9
ghareb <otmaneanna2020@gmail.com>
2025-03-17 20:55:52 +0000

    add the changement in the file index.css

--- src/components/Nav/Footer/Footer..js
+++ src/components/Nav/Footer/Footer..js
@@ -1,18 +1,19 @@
 import React from "react";
+import './Footer.css'
 import footerlogo from "../../../Assists/logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
-import facebook from '../../../Assists/téléchargement.png'
-import twitter from '../../../Assists/téléchargement (1).png'
-import instagram from '../../../Assists/téléchargement (2).png'
-import linkedin from '../../../Assists/téléchargement (3).png'
+import facebook from "../../../Assists/téléchargement.png";
+import twitter from "../../../Assists/téléchargement (1).png";
+import instagram from "../../../Assists/téléchargement (2).png";
+import linkedin from "../../../Assists/téléchargement (3).png";
 
 const Footer = () => {
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-3 col-sm-6">
-          <img src={footerlogo} />
+          <img src={footerlogo} className="footerlogo"  />
           <p>
             Lorem ipsum is dolor sit amet, csectetur adipiscing elit, dolore
             smod tempor incididunt ut labore et
@@ -28,41 +29,79 @@ const Footer = () => {
           </div>
         </div>
         <div className="col-md-3 col-sm-6">
-            <h2>Quick Links</h2>
-            <ul>
-                <li><a href="#"></a>About Us</li>
-                <li><a href="#"></a>Services</li>
-                <li><a href="#"></a>Booking</li>
-                <li><a href="#"></a>Faq's</li>
-                <li><a href="#"></a>Blogs</li>
-                <li><a href="#"></a>Out Team</li>
-            </ul>
+          <h2>Quick Links</h2>
+          <ul>
+            <li>
+              <a href="#">About Us</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+            </li>
+            <li>
+              <a href="#">Booking</a>
+            </li>
+            <li>
+              <a href="#">Faq's</a>
+            </li>
+            <li>
+              <a href="#">Blogs</a>
+            </li>
+            <li>
+              <a href="#">Out Team</a>
+            </li>
+          </ul>
         </div>
         <div className="col-md-3 col-sm-6">
-        <h2>Our Service</h2>
-            <ul>
-                <li><a href="#"></a>Dental Care</li>
-                <li><a href="#"></a>Cardiac Clinic</li>
-                <li><a href="#"></a>Massege Therapy</li>
-                <li><a href="#"></a>Cardiology</li>
-                <li><a href="#"></a>Precise Diagnosis</li>
-                <li><a href="#"></a>Abmbulance Services</li>
+          <h2>Our Service</h2>
+          <ul>
+            <li>
+              <a href="#">Dental Care</a>
+            </li>
+            <li>
+              <a href="#">Cardiac Clinic</a>
+            </li>
+            <li>
+              <a href="#">Massege Therapy</a>
+            </li>
+            <li>
+              <a href="#">Cardiology</a>
+            </li>
+            <li>
+              <a href="#">Precise Diagnosis</a>
+            </li>
+            <li>
+              <a href="#">Abmbulance Services</a>
+            </li>
+          </ul>
+          <div className="col-md-3 col-sm-6">
+            <h2>Subscribe</h2>
+            <form>
+              <input type="email" />
+              <button type="submit">subscribe now</button>
+            </form>
+            <ul className="social">
+              <li>
+                <a href="#">
+                  <img src={facebook} />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src={twitter} />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src={instagram} />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src={linkedin} />
+                </a>
+              </li>
             </ul>
-            <div className="col-md-3 col-sm-6">
-                <h2>Subscribe</h2>
-                <form>
-                    <input type="email"/>
-                    <button type="submit">subscribe now</button>
-                    
-                </form>
-                <ul className="social">
-                    <li><a href="#"><img src={facebook}/></a></li>
-                    <li><a href="#"><img src={twitter}/></a></li>
-                    <li><a href="#"><img src={instagram}/></a></li>
-                    <li><a href="#"><img src={linkedin}/></a></li>
-
-                </ul>
-            </div>
+          </div>
         </div>
       </div>
     </div>
