61cde0c7d2f4e4367b95ec4c9b33a6f665d49805
ghareb <otmaneanna2020@gmail.com>
2025-03-19 23:15:38 +0000

    changes in file Contact.js and Banner.css and js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import { Fragment } from "react";
+
 import "./App.css";
 import Home from "./Pages/Home.";
 
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import Contact from "./Pages/Contact";
 import Layout from "./components/Nav/Layout/Layout";
 
-const routes = createBrowserRouter(createRoutesFromElements(
-  <Route path="/Layouat"  element={<Layout/>}>
-    <Route index path="/home" element={<Home/>}/>
-    <Route path="/contact" element={<Contact/>}/>
-  </Route>
-))
+const routes = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/Layouat" element={<Layout />}>
+      <Route index path="/home" element={<Home />} />
+      <Route path="/contact" element={<Contact />} />
+    </Route>
+  )
+);
 
 function App() {
   return (
     <Fragment>
-      <RouterProvider router={routes}/>
+      <RouterProvider router={routes} />
     </Fragment>
-     
-   ) ;
+  );
 }
 
 export default App;
--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -4,7 +4,25 @@ import Banner from "../components/Nav/Banner/Banner";
 const Contact = () => {
   return (
     <>
-      <Banner />
+      <Banner title="Contact" smtitle="Contact"/>
+      <section className="contact-us">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-6 col-md-6">
+                    <form>
+                        <div className="form-control">
+                            <input placeholder="Your Name"/>
+
+                        </div>
+                    </form>
+                    
+                </div>
+                <div className="col-lg-6 col-md-6">
+
+                </div>
+            </div>
+        </div>
+      </section>
     </>
   );
 };
