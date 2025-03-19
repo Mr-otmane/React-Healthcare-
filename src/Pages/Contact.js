7f2d052d4411650d5a220441255b365a410695d6
ghareb <otmaneanna2020@gmail.com>
2025-03-19 23:22:38 +0000

    add a contact in this code

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -4,23 +4,35 @@ import Banner from "../components/Nav/Banner/Banner";
 const Contact = () => {
   return (
     <>
-      <Banner title="Contact" smtitle="Contact"/>
+      <Banner title="Contact" smtitle="Contact" />
       <section className="contact-us">
         <div className="container">
-            <div className="row">
-                <div className="col-lg-6 col-md-6">
-                    <form>
-                        <div className="form-control">
-                            <input placeholder="Your Name"/>
-
-                        </div>
-                    </form>
-                    
+          <div className="row">
+            <div className="col-lg-6 col-md-6">
+              <form>
+                <div className="form-control">
+                  <input placeholder="Your Name" />
                 </div>
-                <div className="col-lg-6 col-md-6">
-
+                <div className="form-control">
+                  <input placeholder="Your Email" />
                 </div>
+                <div className="form-control">
+                  <input placeholder="Phone Numbers" />
+                </div>
+                <select>
+                  <option>Select Department</option>
+                  <option>One</option>
+                  <option>Two</option>
+                  <option>Three</option>
+                </select>
+                <div className="form-control">
+                  <textarea placeholder="Message"></textarea>
+                </div>
+                <button>submit</button>
+              </form>
             </div>
+            <div className="col-lg-6 col-md-6"></div>
+          </div>
         </div>
       </section>
     </>
