c029cb0d46a10da657d68bbdd2291311534ff9b5
ghareb <otmaneanna2020@gmail.com>
2025-03-20 06:16:57 +0000

    add photo in file banner.css and add the style in the same file

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,5 +1,10 @@
 import React from "react";
 import Banner from "../components/Nav/Banner/Banner";
+import { faMap } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import './Contact.css'
+
+
 
 const Contact = () => {
   return (
@@ -31,7 +36,18 @@ const Contact = () => {
                 <button>submit</button>
               </form>
             </div>
-            <div className="col-lg-6 col-md-6"></div>
+            <div className="col-lg-6 col-md-6">
+              <div className="side">
+                <div className="overlay">
+                  <h3>Contact Us For Any Informations</h3>
+                  <li>
+                    <FontAwesomeIcon icon={faMap} />
+                    Location
+                  </li>
+                  <hr />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
